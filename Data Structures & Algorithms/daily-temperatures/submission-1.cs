05787943584b9fcfe933fcc865017e8e2ccbf5b3
Eric <36512385+PhucNghi176@users.noreply.github.com>
2026-06-03 15:27:29 +0700

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] DailyTemperatures(int[] tm) {
+        var list = new int[tm.Length];
+        for(int i =0;i<tm.Length;i++){
+            int c =0;
+            for(int j = i+1;j<tm.Length;j++){
+                if(tm[i]<tm[j]){
+                    c+=1;
+                    list[i]=c;
+                    break;
+                }
+                else
+                c++;
+            }
+        }
+        return list;
+    }
+}
